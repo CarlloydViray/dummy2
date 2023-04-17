4fe63728216d785bcdaec287f09bb49316056a97
Carlloyd Viray <viraycarlloyd@gmail.com>
2023-04-17 16:20:59 +0000

    cplusplus

---
+++ lib/index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string input;
+
+    cout<<"Enter name: "<<endl;
+    cin>>input;
+
+    cout<<"Input is "<<input<<endl;
+
+    //
+    return 0;
+}
